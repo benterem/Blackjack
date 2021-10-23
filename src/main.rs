4007471d79507150c62ddf44c2e25c175ef2a17a
benterem <benterem@gmail.com>
2021-10-23 15:16:27 +0300

    generate random card

--- src/main.rs
+++ src/main.rs
@@ -12,18 +12,39 @@ fn main() {
         .read_line(&mut num_decks)
         .expect("Failed to read line");
 
-    draw_card();
-    draw_card();
-    draw_card();
-
-
-    //init hashmap
-
+    let num_decks:u8 = num_decks.trim().parse().expect("Please type a number");
+    
+    draw_card(num_decks)
 }
 
 
-fn draw_card() {
-    let deck = rand::thread_rng().gen_range(0..6);
-    let suit = rand::thread_rng().gen_range(0..4);
-    let rank = rand::thread_rng().gen_range(1..14);
+fn draw_card(num_decks: u8) {
+    let deck:u8 = rand::thread_rng().gen_range(0..num_decks);
+    let suit:u8 = rand::thread_rng().gen_range(0..=3);
+    let rank:u8 = rand::thread_rng().gen_range(1..14);
+
+    println!("{} {} {}", rank, suit, deck);
+
+    let mut card = String::from("");
+
+    match rank {
+        1 => card.push_str("Ace"),
+        11 => card.push_str("Prince"),
+        12 => card.push_str("Queen"),
+        13 => card.push_str("King"),
+        other => card.push_str(&other.to_string())
+    }
+    
+    
+    match suit {
+        0 => card.push_str(" of Hearts"),
+        1 => card.push_str(" of Spades"),
+        2 => card.push_str(" of Diamonds"),
+        _ => card.push_str(" of Clubs"),
+    }
+
+    card.push_str(&deck.to_string());
+    
+    println!("{}", card);
+    // cards_drawn.insert(card, true);
 }
